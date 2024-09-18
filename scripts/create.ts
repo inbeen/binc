2b159dcfb46e961fe9a7b581bb187041fe453a75
inbeen <309573307@qq.com>
2024-09-18 16:59:39 +0800

    chore: create 脚本调整

--- scripts/create.ts
+++ scripts/create.ts
@@ -1,23 +1,11 @@
-#!/usr/bin/env node
-import { program } from 'commander';
-import inquirer from 'inquirer';
+import { input } from '@inquirer/prompts';
 import chalk from 'chalk';
 import { writeFileSync, unlinkSync } from 'node:fs';
 import { readdir } from 'node:fs/promises';
 import { copy } from 'fs-extra';
 import { resolve } from 'node:path';
 import { renderFile } from 'ejs';
-
-const questions = [
-  {
-    type: 'input',
-    name: 'component',
-    message: 'Please enter the name of component. (eg. xx-xxx)',
-    default() {
-      return 'input';
-    }
-  }
-];
+import { isKebabCase, kebab2Pascal } from './helpers/format';
 
 const pkgList = [
   { from: 'package.ejs', to: 'package.json' },
@@ -35,15 +23,7 @@ const storyList = [
 
 const preserveNames = ['guide'];
 
-const isKebabCase = (name) => /^[a-z]+(-[a-z]+)*$/.test(name);
-
-const kebab2Pascal = (name) =>
-  name
-    .split('-')
-    .map((s) => s.replace(/\b[a-z]/g, (match) => match.toUpperCase()))
-    .join('');
-
-const validate = (name, dirs) => {
+const validate = (name: string, dirs: string[]) => {
   // duplicate
   if (dirs.includes(name)) {
     console.log(chalk.red(`'${name}' already exists, please enter again.`));
@@ -56,17 +36,17 @@ const validate = (name, dirs) => {
   return true;
 };
 
-const renderPkg = (name, comp) => {
+const renderPkg = (name: string, comp: string) => {
   Promise.all(
     pkgList.map(
       (pkg) =>
         new Promise((res) => {
           const source = resolve(`./packages/${name}/${pkg.from}`);
           const target = resolve(`./packages/${name}/${pkg.to}`);
-          renderFile(source, { name, component: comp }, null, function (_, str) {
+          renderFile(source, { name, component: comp }, {}, function (_, str) {
             writeFileSync(target, str);
             unlinkSync(source);
-            res();
+            res(undefined);
           });
         })
     )
@@ -75,17 +55,17 @@ const renderPkg = (name, comp) => {
   });
 };
 
-const renderStory = (name, comp) => {
+const renderStory = (name: string, comp: string) => {
   Promise.all(
     storyList.map(
       (story) =>
         new Promise((res) => {
           const source = resolve(`./stories/${comp}/${story.from}`);
           const target = resolve(`./stories/${comp}/${story.to.replace('Demo', comp)}`);
-          renderFile(source, { name, component: comp }, null, function (_, str) {
+          renderFile(source, { name, component: comp }, {}, function (_, str) {
             writeFileSync(target, str);
             unlinkSync(source);
-            res();
+            res(undefined);
           });
         })
     )
@@ -94,7 +74,7 @@ const renderStory = (name, comp) => {
   });
 };
 
-const generate = (name) => {
+const generate = (name: string) => {
   const comp = kebab2Pascal(name);
   const sourcePkg = resolve('./templates/package');
   const targetPkg = resolve(`./packages/${name}`);
@@ -114,10 +94,11 @@ const generate = (name) => {
 };
 
 const interaction = () => {
-  inquirer
-    .prompt(questions)
-    .then(async (answers) => {
-      const { component: name } = answers;
+  input({
+    message: 'Please enter the name of component. (eg. xx-xxx)',
+    required: true
+  })
+    .then(async (name: string) => {
       const dirs = await readdir(resolve(process.cwd(), 'packages'));
       if (!validate(name, dirs.concat(preserveNames))) {
         interaction();
@@ -128,11 +109,5 @@ const interaction = () => {
     .catch(() => {});
 };
 
-program
-  .command('create')
-  .description('New component')
-  .action(() => {
-    interaction();
-  });
-
-program.parse(process.argv);
+console.log(chalk.green('=== New component ==='));
+interaction();
--- scripts/helpers/format.ts
+++ scripts/helpers/format.ts
@@ -1,4 +1,21 @@
 /**
+ * check kebab-case
+ */
+export function isKebabCase(name: string) {
+  return /^[a-z]+(-[a-z]+)*$/.test(name);
+}
+
+/**
+ * kebab-case => PascalCase
+ */
+export function kebab2Pascal(name: string) {
+  return name
+    .split('-')
+    .map((s) => s.replace(/\b[a-z]/g, (match) => match.toUpperCase()))
+    .join('');
+}
+
+/**
  * 格式化 package.json 的 name，返回 kebab-case, camelCase, PascalCase
  * name 格式为 @binc/xx-xxx
  */
